2f5f9a1e6c2f5ea441cfdd59b031f7cb2e628dd5
Sebastian Silva Mendez <sebastian.silvam@perficient.com>
2022-07-26 03:23:37 -0500

    users

---
+++ src/main/java/com/proyecto/bootcamp/Controllers/UsuarioController.java
@@ -0,0 +1,35 @@
+package com.proyecto.bootcamp.Controllers;
+
+import java.util.List;
+
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.proyecto.bootcamp.Services.UsuarioServices;
+import com.proyecto.bootcamp.Services.DTO.UserDTO.UsuarioDTO;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+    @Autowired
+    UsuarioServices services;
+
+    @GetMapping()
+    public List<UsuarioDTO> getUsuariosPagination(@RequestParam(defaultValue = "0",required = false) @PositiveOrZero(message = "{page.zero}") Integer page,
+    @RequestParam(defaultValue = "100",required = false) @Positive(message = "{size.positive}") Integer size){
+        return services.getUsuariosPaginated(page, size);    
+    }
+
+    @PostMapping()
+    public UsuarioDTO postMateria(@RequestBody UsuarioDTO usuarioDTO){
+       return services.saveUsuario(usuarioDTO);
+    }
+}
---
+++ src/main/java/com/proyecto/bootcamp/Services/UsuarioServices.java
@@ -0,0 +1,58 @@
+package com.proyecto.bootcamp.Services;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.StreamSupport;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.proyecto.bootcamp.DAO.Models.Usuario;
+import com.proyecto.bootcamp.DAO.Repositories.UsuarioCrudRepository;
+import com.proyecto.bootcamp.Services.DTO.UserDTO.UsuarioDTO;
+import com.proyecto.bootcamp.Services.Mapper.UsuarioMapper;
+
+public class UsuarioServices{
+    @Autowired
+    private UsuarioCrudRepository repository;
+
+    UsuarioMapper mapper = UsuarioMapper.INSTANCE;
+
+    public UsuarioDTO saveUsuario(UsuarioDTO usuarioDTO) {
+        Usuario usuarioEntity = mapper.dtoToUsuario(usuarioDTO);
+        // usuarioEntity.setClave(new BCryptPasswordEncoder().encode(usuarioEntity.getClave()));
+        repository.save(usuarioEntity);
+        
+        return mapper.usuarioToDTO(usuarioEntity);
+    }
+
+    public Usuario getUsuarioByCorreo(String correo) {
+        Optional<Usuario> usuario = repository.findByCorreo(correo);
+        return usuario.get();
+    }
+
+    public List<UsuarioDTO> getUsuariosPaginated(int page, int size){
+        List<UsuarioDTO> usuarios = StreamSupport.stream(repository.findAllPaginated(size, (page)*size).spliterator(), false)
+                                            .map(mapper::usuarioToDTO)
+                                            .toList();
+        return usuarios;      
+    }
+
+    // @Override
+    // public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    //     Usuario usuario = getUsuarioByCorreo(username);
+
+    //     Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+    //     authorities.add(new SimpleGrantedAuthority(usuario.getRol()));
+    //     return new org.springframework.security.core.userdetails.User(usuario.getCorreo(), usuario.getClave(), authorities);
+    // }
+}
