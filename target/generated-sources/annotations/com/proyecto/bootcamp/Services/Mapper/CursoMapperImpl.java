8b0f4493bc53223ae7b2b8ffa574fcf469c28a4a
Sebastian Silva Mendez <sebastian.silvam@perficient.com>
2022-07-28 08:49:17 -0500

    to merge

--- target/generated-sources/annotations/com/proyecto/bootcamp/Services/Mapper/CursoMapperImpl.java
+++ target/generated-sources/annotations/com/proyecto/bootcamp/Services/Mapper/CursoMapperImpl.java
@@ -12,7 +12,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-07-27T10:23:22-0500",
+    date = "2022-07-28T08:47:02-0500",
     comments = "version: 1.5.2.Final, compiler: Eclipse JDT (IDE) 1.4.100.v20220318-0906, environment: Java 17.0.3 (Eclipse Adoptium)"
 )
 public class CursoMapperImpl implements CursoMapper {
@@ -25,10 +25,10 @@ public class CursoMapperImpl implements CursoMapper {
 
         CursoDTO cursoDTO = new CursoDTO();
 
-        cursoDTO.setId( curso.getId() );
-        cursoDTO.setNombre( curso.getNombre() );
         cursoDTO.setDescripcion( curso.getDescripcion() );
+        cursoDTO.setId( curso.getId() );
         cursoDTO.setMaterias( materiaSetToMateriaDTOList( curso.getMaterias() ) );
+        cursoDTO.setNombre( curso.getNombre() );
 
         return cursoDTO;
     }
@@ -41,10 +41,10 @@ public class CursoMapperImpl implements CursoMapper {
 
         Curso curso = new Curso();
 
-        curso.setId( cursoDTO.getId() );
-        curso.setNombre( cursoDTO.getNombre() );
         curso.setDescripcion( cursoDTO.getDescripcion() );
+        curso.setId( cursoDTO.getId() );
         curso.setMaterias( materiaDTOListToMateriaSet( cursoDTO.getMaterias() ) );
+        curso.setNombre( cursoDTO.getNombre() );
 
         return curso;
     }
@@ -56,11 +56,11 @@ public class CursoMapperImpl implements CursoMapper {
 
         MateriaDTO materiaDTO = new MateriaDTO();
 
-        materiaDTO.setId( materia.getId() );
         materiaDTO.setDia( materia.getDia() );
-        materiaDTO.setHora( materia.getHora() );
         materiaDTO.setFechafin( materia.getFechafin() );
         materiaDTO.setFechainicio( materia.getFechainicio() );
+        materiaDTO.setHora( materia.getHora() );
+        materiaDTO.setId( materia.getId() );
 
         return materiaDTO;
     }
@@ -85,11 +85,11 @@ public class CursoMapperImpl implements CursoMapper {
 
         Materia materia = new Materia();
 
-        materia.setId( materiaDTO.getId() );
         materia.setDia( materiaDTO.getDia() );
-        materia.setHora( materiaDTO.getHora() );
         materia.setFechafin( materiaDTO.getFechafin() );
         materia.setFechainicio( materiaDTO.getFechainicio() );
+        materia.setHora( materiaDTO.getHora() );
+        materia.setId( materiaDTO.getId() );
 
         return materia;
     }
