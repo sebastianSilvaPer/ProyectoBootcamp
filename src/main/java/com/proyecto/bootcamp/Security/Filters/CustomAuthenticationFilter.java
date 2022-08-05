3b7c6ecee662f9e2f286123291603bd240bfad59
Sebastian Silva Mendez <sebastian.silvam@perficient.com>
2022-08-05 08:19:18 -0500

    stable version v2

--- src/main/java/com/proyecto/bootcamp/Security/Filters/CustomAuthenticationFilter.java
+++ src/main/java/com/proyecto/bootcamp/Security/Filters/CustomAuthenticationFilter.java
@@ -2,9 +2,7 @@ package com.proyecto.bootcamp.Security.Filters;
 
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -16,8 +14,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.encrypt.TextEncryptor;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
